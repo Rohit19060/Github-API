528d69a6ab71ee330699d51e3832d62c0c22f1fe
Rohit Jain <rohitjain19060@gmail.com>
2021-10-05 08:31:09 +0530

    :octocat: Refactored a Bit

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-"use strict";
-
-const searchURL =
-  "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/search/NewsSearchAPI";
-
 const formatQueryParams = (params) => {
   const queryItems = Object.keys(params).map(
     (key) => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
@@ -11,10 +6,8 @@ const formatQueryParams = (params) => {
 };
 
 const displayResults = (responseJson, maxResults) => {
-  console.log(responseJson);
   $("#results-list").empty();
   for (let i = 0; (i < responseJson.length) & (i < maxResults); i++) {
-    console.log(responseJson[i]);
     $("#results-list").append(
       `<li>
       <h3><a href="${responseJson[i].html_url}" target="_blank" title="${responseJson[i].name}">${responseJson[i].name}</a></h3>
